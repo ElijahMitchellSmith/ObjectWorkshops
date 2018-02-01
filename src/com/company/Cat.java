8189cc326485865fe4325ed2a3db9a962d982d6a
Elijah Smith <elijahsmith@Interapts-MacBook-Pro-71.local>
2018-02-01 14:45:24 -0500

    Created new class for obj

---
+++ src/com/company/Cat.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Cat {
+    private int legs;
+    private boolean fur;
+    private String color;
+    private Scanner input = new Scanner(System.in);
+
+    public Cat() {
+//        System.out.println("Does you cat have fur?");
+//        setFur(input.nextBoolean());
+//        System.out.println("What color would you like your cat?");
+//        setColor(input.nextLine());
+//        System.out.println("How many legs does your cat have?");
+//        setLegs(input.nextInt());
+
+
+    }
+}
+
+//    public int getLegs() {
+//        return legs;
+//    }
+//
+//    public void setLegs(int legs) {
+//        this.legs = legs;
+//    }
+//
+//    public boolean isFur() {
+//        return fur;
+//    }
+//
+//    public void setFur(boolean fur) {
+//        this.fur = fur;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Cat cat = new Cat();
+//        System.out.println(cat.getColor());
+//        System.out.println(cat.getLegs());
+//        System.out.println(cat.isFur());
+
+        Pencil pencil = new Pencil(.7,"papermate",true,"yellow",true);
+        Pencil pencil1 = new Pencil(.8,"Dixon",false,"Black",true);
+
+        System.out.println("This is pencil:" + pencil.getColor());
+        System.out.println("This is pencil 1:" + pencil1.getColor());
+
+
+
     }
 }
